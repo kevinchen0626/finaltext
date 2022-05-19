6e6dd1453b349826ba2aa6e92c8036617f596ede
kevinchen0626 <s1091804@gm.pu.edu>
2022-05-19 16:33:57 +0800

    stage3

--- app/src/main/java/tw/edu/pu/s1091804/midtext0/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1091804/midtext0/MainActivity.kt
@@ -5,9 +5,15 @@ import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
 import kotlinx.android.synthetic.main.activity_main.*
 import tw.edu.pu.s1091804.midtext0.databinding.ActivityMainBinding
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 
 class MainActivity : AppCompatActivity(){
@@ -18,6 +24,7 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         lateinit var job: Job
         super.onCreate(savedInstanceState)
+
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -52,9 +59,16 @@ class MainActivity : AppCompatActivity(){
                 delay(25)
             }
         }
+        val img: ImageView = findViewById(R.id.img2)
+        GlideApp.with(this)
+            .load(R.drawable.mid)
+            .circleCrop()
+            .override(800, 600)
+            .into(img2)
 
         //設定螢幕水平顯示
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
 
     }
 
